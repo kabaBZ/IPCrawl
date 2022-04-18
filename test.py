c3983f39979198289125822b30196bc986140fd8
kaba <1137088773@qq.com>
2022-04-18 18:12:46 +0800

    update

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-from
+from IPCrawl import IPCrawl
+
+a = IPCrawl.FatezeroIP()
+b = IPCrawl.Ip3366()
+d = IPCrawl.JiangxianliIP()
+e = IPCrawl.SeofangfaProxy()
+f = IPCrawl.Ip66Proxy()
+g = IPCrawl.TaiyangProxy()
+h = IPCrawl.ZdayeProxy()
+i = IPCrawl.YqieProxy()
+c = IPCrawl.KuaidailiProxy()
+
+a.proxy_to_redis()
+b.proxy_to_redis()
+c.proxy_to_redis()
+d.proxy_to_redis()
+e.proxy_to_redis()
+f.proxy_to_redis()
+g.proxy_to_redis()
+# h.proxy_to_redis()
+i.proxy_to_redis()
